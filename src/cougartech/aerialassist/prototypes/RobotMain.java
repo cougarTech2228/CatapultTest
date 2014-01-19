525b0b092490d2c60816605010c484758913b559
Michael <>
2014-01-19 12:48:25 -0500

    Outline of catapult prototype

---
+++ src/cougartech/aerialassist/prototypes/RobotMain.java
@@ -0,0 +1,19 @@
+package cougartech.aerialassist.prototypes;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class RobotMain extends IterativeRobot
+{
+
+    public void robotInit()
+    {
+    }
+
+    public void autonomousPeriodic()
+    {
+    }
+
+    public void teleopPeriodic()
+    {
+    }
+}
