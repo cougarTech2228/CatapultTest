38043f178f9457b8ee92bbd7b10243786cf80c17
Michael <>
2014-01-19 13:53:52 -0500

    Motor on off outline

--- src/cougartech/aerialassist/prototypes/RobotMain.java
+++ src/cougartech/aerialassist/prototypes/RobotMain.java
@@ -1,12 +1,25 @@
 package cougartech.aerialassist.prototypes;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import java.util.Date;
 
 public class RobotMain extends IterativeRobot
 {
 
+    Joystick joy;
+    Talon motor;
+    Date nowTime;
+    boolean wantShoot = false;
+    long sTime;
+    int motorTimeOut = 1000;
+        
     public void robotInit()
     {
+        joy = new Joystick(1);
+        motor = new Talon(1);
+        nowTime = new Date();
     }
 
     public void autonomousPeriodic()
@@ -15,5 +28,26 @@ public class RobotMain extends IterativeRobot
 
     public void teleopPeriodic()
     {
+        if(wantShoot)
+        {
+            if((sTime + motorTimeOut) > nowTime.getTime())
+            {
+                motor.set(0.0);
+            }
+            else
+            {
+                
+            }
+        }
+        else if(!wantShoot && joy.getRawButton(1))
+        {
+            sTime = nowTime.getTime();
+            motor.set(1.0);
+            wantShoot = true;
+        }
+        else
+        {
+            motor.set(0.0);
+        }
     }
 }
