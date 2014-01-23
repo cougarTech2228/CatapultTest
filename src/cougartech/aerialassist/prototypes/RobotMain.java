6342fe563dfba720f633a92e12c5b1a25fc29d88
Michael <>
2014-01-22 20:35:41 -0500

    Reset and ball detect is so perfect!

--- src/cougartech/aerialassist/prototypes/RobotMain.java
+++ src/cougartech/aerialassist/prototypes/RobotMain.java
@@ -1,7 +1,7 @@
 package cougartech.aerialassist.prototypes;
 
 import edu.wpi.first.wpilibj.CANJaguar;
-//import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
@@ -12,33 +12,30 @@ public class RobotMain extends IterativeRobot
 
     Joystick joy;
     CANJaguar motor;
+    CANJaguar motor2;
+    DigitalInput stopSwitch;
+    DigitalInput ballDetect;
     Date nowTime;
     boolean wantShoot = false;
-
     boolean reset = false;
     long sTime;
-    long pTime;
-    int movementPer = 61;
-    long motorTimeOut = 5000;
     long timePer = 120;
-    double scaledCurrent;
-    double resistance;
         
     public void robotInit()
     {
         joy = new Joystick(1);
+        stopSwitch = new DigitalInput(1);
+        ballDetect = new DigitalInput(2);
         
         try
         {
             motor = new CANJaguar(10);
+            motor = new CANJaguar(11);
         }
         catch(CANTimeoutException ex)
         {
             ex.printStackTrace();
         }
-        
-        nowTime = new Date();
-        //encoder = new Encoder(1, 2);
     }
 
     public void autonomousPeriodic()
@@ -52,6 +49,7 @@ public class RobotMain extends IterativeRobot
         try
         {
             motor.enableControl();
+            motor2.enableControl();
         }
         catch(CANTimeoutException ex)
         {
@@ -65,6 +63,19 @@ public class RobotMain extends IterativeRobot
                     try
                     {
                         motor.setX(1.0);
+                        motor2.setX(1.0);
+                    }
+                    catch(CANTimeoutException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        motor.setX(0.0);
+                        motor2.setX(0.0);
                     }
                     catch(CANTimeoutException ex)
                     {
@@ -72,26 +83,43 @@ public class RobotMain extends IterativeRobot
                     }
                     reset = true;
                 }
-                else
-                {
-                    try
-                    {
-                        motor.setX(0.0);
-                    }
-                    catch(CANTimeoutException ex)
-                    {
-                        ex.printStackTrace();
-                    }
-                }
          }
-        else if(!wantShoot && joy.getRawButton(1))
+        else if(!wantShoot && joy.getRawButton(1) && !ballDetect.get())
         {
             sTime = nowTime.getTime();
             wantShoot = true;
-            
         }
-        
+       
         if(reset)
+        {
+            if(stopSwitch.get())
+            {
+                try
+                {
+                    motor.setX(0.0);
+                    motor2.setX(0.0);
+                }
+                catch(CANTimeoutException ex)
+                {
+                    ex.printStackTrace();
+                }
+                reset  = false;
+                wantShoot = false;
+            }
+            else
+            {
+                try
+                {
+                    motor.setX(-0.25);
+                    motor2.setX(-0.25);
+                }
+                catch(CANTimeoutException ex)
+                {
+                    ex.printStackTrace();
+                }                
+            }
+        }
+        else
         {
             
         }
